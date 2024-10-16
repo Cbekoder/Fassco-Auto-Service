f9075529a4fc62ac8a2f939d2b15a8e5a74c70f4
cbekoder <cbekoder@gmail.com>
2024-10-16 11:16:38 +0000

    host update

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-lvwp=j+%=m4s+$2ji%kurxiwq(-9sb(c&fq$$_@)y*nrljb1sc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
